46c27b96b528b300436a3f7c0cdd45aa6684b59a
Riddhi <riddhicjain242@gmail.com>
2025-12-29 03:13:59 +0530

    Fix React import issues, route ordering, graph loading performance, and data fetching

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -1,10 +1,10 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyAe02H7fdhC8KSY1Q_zqK8sbjMwM2ZmrBU",
-    authDomain: "prior-73a29.firebaseapp.com",
-    projectId: "prior-73a29",
-    storageBucket: "prior-73a29.appspot.com",
-    messagingSenderId: "1006044615950",
-    appId: "1:1006044615950:web:1a007d0694803b1791335f"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID
   };
 
   export default firebaseConfig;
